820d7929e253e4ea8bef42d2abf47d71fde79d87
Daniel Rosales <drosales9135@gmail.com>
2020-12-07 15:47:28 -0500

    Runner method added constr added for Goblin and Hero

--- Goblin.java
+++ Goblin.java
@@ -1,5 +1,37 @@
 package goblin_Tower;
 
 public class Goblin {
+	int maxHealth;
+	int attackPower;
+	int defensePower;
+	int currHealth;
+
+	public Goblin() {
+		maxHealth = (int) (Math.random() * (10 - 5) + 5);
+		attackPower = (int) (Math.random() * (3 - 2) + 2);
+		defensePower = (int) (Math.random() * (5 - 1) + 1);
+		currHealth = maxHealth;
+
+	}
+
+	public void setcurrHealth(int x) {
+		currHealth = x;
+	}
+
+	public int getcurrHealth() {
+		return currHealth;
+	}
+
+	public int getMaxHealth() {
+		return maxHealth;
+	}
+
+	public int getattackPower() {
+		return attackPower;
+	}
+
+	public int getdefensePower() {
+		return defensePower;
+	} 
 
 }
